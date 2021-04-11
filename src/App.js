c4864ac33fb2ec5241b60ea48a258607da009256
Marika <marikashanahan@gmail.com>
2021-04-11 17:33:46 -0600

    Create Navigation component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Route, NavLink } from 'react-router-dom'
+import Navigation from './components/Navigation/Navigation'
 import './App.css';
 
 class App extends Component {
@@ -17,16 +18,15 @@ class App extends Component {
         <Route exact path="/">
           <h1>CarryOkay</h1>
           <p>Hello friend 😬</p>
-          <nav className="home-nav">
-            <NavLink to="/mysongs" >My Songs</NavLink>
-            <NavLink to="/songbook">Songbook</NavLink>
-          </nav>
+          <Navigation class="home-nav" />
         </Route>
         <Route path="/mysongs">
           <p>Made it to mysongs</p>
+          <Navigation class="mysongs-nav" />
         </Route>
         <Route path="/songbook">
           <p>Made it to songbook</p>
+          <Navigation class="songbook-nav" />
         </Route>
       </div>
     )
