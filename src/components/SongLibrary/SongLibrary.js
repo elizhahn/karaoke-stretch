e3f4c0c68858637ad89832340603595d9a0d537b
Marika <marikashanahan@gmail.com>
2021-04-12 14:01:15 -0600

    Import SongLibrary.css

--- src/components/SongLibrary/SongLibrary.js
+++ src/components/SongLibrary/SongLibrary.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import SongCard from '../SongCard/SongCard';
+import '../SongLibrary/SongLibrary.css'
 
 const SongLibrary = ({ songs }) => {
   const allSongs = songs.map(song => {
     return (
       <SongCard 
+      className="song-card"
       title={ song.title }
       artist={ song.artist }
       genres={ song.genres }
