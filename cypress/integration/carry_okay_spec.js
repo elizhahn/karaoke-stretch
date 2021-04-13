24bfce7433d8184b53ba669170ef50017326f7c1
Jessica Justice <jessbjust@gmail.com>
2021-04-12 19:27:42 -0600

    Build out tests for App Load

--- cypress/integration/carry_okay_spec.js
+++ cypress/integration/carry_okay_spec.js
@@ -2,10 +2,20 @@ describe('CarryOkay', () => {
 
   describe('App Load', () => {
 
+    beforeEach(() => {
+      cy.visit('http://localhost:3000');
+    });
 
+    it('should display App title and greeting', () => {
+      cy.get('h1').contains("CarryOkay");
+      cy.get('p').contains("Hello friend 😬");
+    });
 
-
-
+    it('should contain Navigation links', () => {
+      cy.get('a').contains("My Songs");
+      cy.get('a').contains("Song Book");
+    });
+    
   });
 
   describe('Navigation', () => {
