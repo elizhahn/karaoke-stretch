e00a40b2c918e6542f93efe0dc00e3e2f4b0f9d8
Jessica Justice <jessbjust@gmail.com>
2021-04-12 19:20:32 -0600

    Install Cypress / Build skeleton test file

---
+++ cypress/integration/carry_okay_spec.js
@@ -0,0 +1,38 @@
+describe('CarryOkay', () => {
+
+  describe('App Load', () => {
+
+
+
+
+
+  });
+
+  describe('Navigation', () => {
+
+
+
+
+
+
+  });
+
+  describe('My Songs', () => {
+
+
+
+
+
+
+  });
+
+  describ('Song Book', () => {
+
+
+
+
+
+
+  });
+
+});
