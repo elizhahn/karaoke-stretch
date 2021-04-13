0974d3243cb15fcf392a3fecbe46ccf288da43bb
Jessica Justice <jessbjust@gmail.com>
2021-04-12 20:16:26 -0600

    Build out Navigation tests

--- cypress/integration/carry_okay_spec.js
+++ cypress/integration/carry_okay_spec.js
@@ -26,15 +26,49 @@ describe('CarryOkay', () => {
       cy.visit('http://localhost:3000');
     });
 
-  
+    it('should be able to navigate to My Songs view from Home view', () => {
+      cy.get('a[id="my-songs"]').click();
+      cy.get("CarryOkay").should('not.exist');
+      cy.get("Hello friend 😬").should('not.exist');
+    });
 
+    it('should be able to navigate to Song Book view from Home view', () => {
+      cy.get('a[id="song-book"]').click();
+      cy.get("CarryOkay").should('not.exist');
+      cy.get("Hello friend 😬").should('not.exist');
+    });
 
+    it('should be able to navigate from My Songs view to Home view', () => {
+      cy.get('a[id="my-songs"]').click();
+      cy.expect('true').to.equal('false');
+    });
+
+    it('should be able to navigate from Song Book view to Home view', () => {
+      cy.get('a[id="song-book"]').click();
+      cy.expect('true').to.equal('false');
+    });
+
+    it('should be able to navigate from My Songs view to Song Book view', () => {
+      cy.get('a[id="my-songs"]').click();
+      cy.get('a[id="song-book"]').click();
+      cy.get('p').contains("Made it to songbook");
+      //WILL NEED TO BE REFACTORED WHEN PAGE TITLE IS UPDATED
+    });
+
+    it('should be able to navigate from Song Book view to My Songs view', () => {
+      cy.get('a[id="song-book"]').click();
+      cy.get('a[id="my-songs"]').click();
+      cy.get('p').contains("Made it to mysongs");
+      //WILL NEED TO BE REFACTORED WHEN PAGE TITLE IS UPDATED
+    });
 
   });
 
   describe('My Songs', () => {
 
-
+    // cy.get('h1').contains('My Songs');
+    // cy.get('a[id="my-songs"]').contains("My Songs");
+    // cy.get('a[id="song-book"]').contains("Songbook");
 
 
 
