f61356ea742073270cbe2e37b27713fa7df87802
Jessica Justice <jessbjust@gmail.com>
2021-04-12 21:18:39 -0600

    Add ids to buttons for testing / build out My Songs tests

--- cypress/integration/carry_okay_spec.js
+++ cypress/integration/carry_okay_spec.js
@@ -1,10 +1,10 @@
 describe('CarryOkay', () => {
 
-  describe('App Load', () => {
+ beforeEach(() => {
+  cy.visit('http://localhost:3000');
+ });
 
-    beforeEach(() => {
-      cy.visit('http://localhost:3000');
-    });
+  describe('App Load', () => {
 
     it('should display App title and greeting', () => {
       cy.get('h1').contains("CarryOkay");
@@ -22,9 +22,6 @@ describe('CarryOkay', () => {
 
     //GET REQUEST stub / intercept here
 
-    beforeEach(() => {
-      cy.visit('http://localhost:3000');
-    });
 
     it('should be able to navigate to My Songs view from Home view', () => {
       cy.get('a[id="my-songs"]').click();
@@ -66,18 +63,41 @@ describe('CarryOkay', () => {
 
   describe('My Songs', () => {
 
-    // cy.get('h1').contains('My Songs');
-    // cy.get('a[id="my-songs"]').contains("My Songs");
-    // cy.get('a[id="song-book"]').contains("Songbook");
+    beforeEach(() => {
+      cy.get('a[id="my-songs"]').click();
+    });
 
+    it('should display My Song page components', () => {
+      cy.get('h1').contains('My Songs');
+      cy.get('a[id="my-songs"]').contains("My Songs");
+      cy.get('a[id="song-book"]').contains("Songbook");
+    });
 
+    it(`should display list of user's songs`, () => {
+      cy.get('a[id="song-book"]').click();
+      cy.get('button[id="1"]').click();
+      cy.get('a[id="my-songs"]').click();
+      cy.get('div[class="song-card"]').contains("Radiohead");
+      cy.get('div[class="song-card"]').contains("electronica");
+      cy.get('div[class="song-card"]').contains("alternative rock");
+      cy.get('img').should('exist');
+      cy.get('button[id="1"]').should('not.exist');
 
+      cy.get('a[id="song-book"]').click();
+      cy.get('button[id="2"]').click();
+      cy.get('a[id="my-songs"]').click();
+      cy.get('div[class="song-card"]').contains("Usher");
+      cy.get('div[class="song-card"]').contains("pop");
+      cy.get('div[class="song-card"]').contains("R&B");
+      cy.get('img').should('exist');
+      cy.get('button[id="2"]').should('not.exist');
+    });
 
   });
 
   describe('Song Book', () => {
 
-
+   
 
 
 
