53b50bb417f0f3ba5872eeb6afcef6d77c3a5d9b
Marika <marikashanahan@gmail.com>
2021-04-17 10:39:07 -0600

    Apply dynamic class to Home NavLink

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,14 +3,22 @@ import { NavLink } from 'react-router-dom';
 import './Navigation.scss'
 
 const Navigation = ({ dynamic }) => {
- 
+  const determineHomeLink = () => {
+    if (dynamic !== 'home-nav') {
+      return ''
+    } else {
+      return 'letter-k'
+    }
+  }
+
+
   return (
     <nav className= { dynamic } >
       <div className="home-btns home-my-songs">
         <NavLink activeClassName="mysongs-active"  className="home-link" id="my-songs" to="/mysongs">My Songs</NavLink>
       </div>
-      <div className="letter-k">
-        <NavLink to="/" className="" >To Home</NavLink>
+      <div className={ determineHomeLink() }>
+        <NavLink to="/">To Home</NavLink>
       </div>
       <div className="home-btns home-song-book">
         <NavLink activeClassName="songbook-active" className="home-link" id="song-book" to="/songbook">SongBook</NavLink>
