d0e24543bb39254be48f48bb0897114a95ac1a85
Marika <marikashanahan@gmail.com>
2021-04-17 10:30:29 -0600

    Add NavLink under letter-k div wrapper in Navigation component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -8,7 +8,9 @@ const Navigation = (props) => {
       <div className="home-btns home-my-songs">
         <NavLink activeClassName="mysongs-active"  className="home-link" id="my-songs" to="/mysongs">My Songs</NavLink>
       </div>
-      {/* <div className="letter-k"></div> */}
+      <div className="letter-k">
+        <NavLink to="/" className="" >To Home</NavLink>
+      </div>
       <div className="home-btns home-song-book">
         <NavLink activeClassName="songbook-active" className="home-link" id="song-book" to="/songbook">SongBook</NavLink>
       </div>
