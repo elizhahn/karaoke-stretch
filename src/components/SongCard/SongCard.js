491d6c674b789fe2a773cd70799a52e0b1e9b5d7
Elizabeth Hahn <elizhahn87@gmail.com>
2021-04-17 15:19:35 -0600

    Convert songCard to a class component

--- src/components/SongCard/SongCard.js
+++ src/components/SongCard/SongCard.js
@@ -1,28 +1,35 @@
-import React from 'react';
+import { React, Component } from 'react';
 import '../SongCard/SongCard.scss'
 
 
-const SongCard = ({ id, title, artist, genres, album_cover, handleSong, buttonIcon, isActive }) => {
-
-  const listItems = genres.map(genre => {
+class SongCard extends Component {
+  constructor(props) {
+  super(props)
+  this.state = {
+    lyrics: {}
+  }
+}
+  render() {
+    const { id, title, artist, genres, album_cover, handleSong, buttonIcon, isActive } = this.props
+    const listItems = genres.map(genre => {
+      return (
+        <li className="genre" key={ `${title}-${genre}` } >{ genre }</li>
+      );
+    });
     return (
-      <li className="genre" key={ `${title}-${genre}` } >{ genre }</li>
-    );
-  });
-
-  return (
-    <div className='song-card' data-cy="song-card">
-      <img src={ album_cover } data-cy="album-img" alt={ `${title} by ${artist} album cover art` }/>
-      <article className="song-details">
-        <h2>{ title }</h2>
-        <p>Artist: { artist }</p>
-        <ul className="genres">
-          { listItems }
-        </ul>
-      </article>
-      <button className={"handle-song-btn"} disabled={!isActive} id={id} onClick={() => handleSong(id)} data-cy="song-card-btn">{ isActive ? buttonIcon[0] : buttonIcon[1]}</button>
-    </div>
-  );
-};
+      <div className='song-card' data-cy="song-card">
+        <img src={ album_cover } data-cy="album-img" alt={ `${title} by ${artist} album cover art` }/>
+        <article className="song-details">
+          <h2>{ title }</h2>
+          <p>Artist: { artist }</p>
+          <ul className="genres">
+            { listItems }
+          </ul>
+        </article>
+        <button className={"handle-song-btn"} disabled={!isActive} id={id} onClick={() => handleSong(id)} data-cy="song-card-btn">{ isActive ? buttonIcon[0] : buttonIcon[1]}</button>
+      </div>
+   );
+ };
+}
 
 export default SongCard;
