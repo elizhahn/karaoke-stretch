1abb03dc582a6c261ea4e5c524c0ae990cd058e9
Marika <marikashanahan@gmail.com>
2021-04-19 15:07:41 -0600

    Move App into own component directory with fixed imports

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
-import './partials/_variables.scss'
 import { Route } from 'react-router-dom';
-import Navigation from './components/Navigation/Navigation';
-import SongLibrary from './components/SongLibrary/SongLibrary';
-import MySongLibrary from './components/mySongLibrary/MySongLibrary';
-import './App.scss';
-import { fetchAllSongData } from './APICalls';
+import Navigation from '../Navigation/Navigation';
+import SongLibrary from '../SongLibrary/SongLibrary';
+import MySongLibrary from '../mySongLibrary/MySongLibrary';
+import { fetchAllSongData } from '../../APICalls';
 import { RiHeartAddLine } from 'react-icons/ri';
 import { MdRemoveCircle } from 'react-icons/md';
 import { FaHeart } from 'react-icons/fa';
-import { modifyData } from './utility';
+import { modifyData } from '../../utility';
 
 
 class App extends Component {
