29ec36b87b168a45e60fac4c762ce1812f3c65f4
Elizabeth Hahn <elizhahn87@gmail.com>
2021-04-19 10:16:36 -0600

    Fix formatting and clean up file

--- cypress/integration/carry_okay_spec.js
+++ cypress/integration/carry_okay_spec.js
@@ -17,10 +17,9 @@ describe('CarryOkay', () => {
       cy.get('[data-cy=my-songs-nav]').contains("My Songs");
       cy.get('[data-cy=song-book-nav]').contains("Song Book");
     });
-
   });
 
-  describe.only('Navigation', () => {
+  describe('Navigation', () => {
 
     it('should be able to navigate to My Songs view from Home view', () => {
       cy.get('[data-cy=my-songs-nav]').click();
@@ -60,7 +59,6 @@ describe('CarryOkay', () => {
       cy.get('[data-cy=my-songs-nav]').click();
       cy.url().should('eq', 'http://localhost:3000/mysongs');
     });
-
   });
 
   describe('My Songs', () => {
@@ -73,8 +71,8 @@ describe('CarryOkay', () => {
 
     it('should display My Song page components', () => {
       cy.get('[data-cy=home-nav]').should('exist');
-      cy.get('[data-cy=my-songs-nav]').contains("My Songs");
-      cy.get('[data-cy=song-book-nav]').contains("Song Book");
+      cy.get('[data-cy=my-songs-nav]').contains('My Songs');
+      cy.get('[data-cy=song-book-nav]').contains('Song Book');
     });
 
     it(`should display list of user's songs`, () => {
@@ -82,9 +80,9 @@ describe('CarryOkay', () => {
       cy.get('[data-cy=song-card-btn]').first().click();
       cy.get('[data-cy=my-songs-nav]').click();
       cy.get('[data-cy=song-card]').should("contain", "Jigsaw falling into place")
-        .and("contain", "Radiohead")
-        .and("contain","Electronica")
-        .and("contain","Rock")
+        .and('contain', 'Radiohead')
+        .and('contain','Electronica')
+        .and('contain','Rock');
       cy.get('[data-cy=album-img]').should('exist');
       cy.get('[data-cy=song-card-btn]').should('exist');
       cy.get('[data-cy=microphone-icon]').should('exist');
@@ -94,94 +92,91 @@ describe('CarryOkay', () => {
       cy.get('[data-cy=song-book-nav]').click();
       cy.get('[data-cy=song-card-btn]').eq(1).click();
       cy.get('[data-cy=my-songs-nav]').click();
-      cy.get('[data-cy=song-card]').should("contain", "My Boo")
-        .and("contain", "Usher")
-        .and("contain","Hip Hop")
-        .and("contain","R&B")
+      cy.get('[data-cy=song-card]').should('contain', 'My Boo')
+        .and('contain', 'Usher')
+        .and('contain','Hip Hop')
+        .and('contain','R&B');
       cy.get('[data-cy=song-card-btn]').click();
       cy.get('[data-cy=song-card]').should('not.exist');
     });
-
   });
 
   describe('Song Book', () => {
 
-  beforeEach(() => {
-    cy.intercept( '/genres', {fixture:"genre_data.json"})
-    cy.intercept('/songs', {fixture:"song_data.json"})
-    cy.visit('http://localhost:3000');
-    cy.get('[data-cy=song-book-nav]').click();
+    beforeEach(() => {
+      cy.intercept( '/genres', {fixture:'genre_data.json'});
+      cy.intercept('/songs', {fixture:'song_data.json'})
+      cy.visit('http://localhost:3000');
+      cy.get('[data-cy=song-book-nav]').click();
+      });
+
+    it('should display Song Book page components', () => {
+      cy.get('[data-cy=home-nav]').should('exist');
+      cy.get('[data-cy=my-songs-nav]').contains('My Songs');
+      cy.get('[data-cy=song-book-nav]').contains('Song Book');
+      cy.get('[data-cy=search-form]').should('exist');
+      cy.get('[data-cy=song-card]').should("have.length", 2);
+      cy.get('[data-cy=song-card]').eq(0).contains('Radiohead');
+      cy.get('[data-cy=song-card]').eq(1).contains('Usher');
+      cy.get('[data-cy=song-card]').first().should('contain', 'Jigsaw falling into place')
+      .and('contain', 'Radiohead')
+      .and('contain','Electronica')
+      .and('contain','Rock');
+      cy.get('[data-cy=album-img]').first().should('exist');
+      cy.get('[data-cy=song-card-btn]').first().should('exist');
+      cy.get('[data-cy=microphone-icon]').should('exist');
+      });
+
+    it('should allow user to make a search', () => {
+      cy.get('[data-cy=search-bar]').type('usher');
+      cy.get('[data-cy=search-btn').click()
+      cy.get('[data-cy=song-card]').should('have.length', 1)
+        .and('contain', 'Usher');
+      cy.get('[data-cy=search-bar]').clear();
+      cy.get('[data-cy=search-bar]').type('body');
+      cy.get('[data-cy=search-btn').click();
+      cy.get('[data-cy=song-card]').should('have.length', 2);
     });
 
-  it('should display Song Book page components', () => {
-    cy.get('[data-cy=home-nav]').should('exist');
-    cy.get('[data-cy=my-songs-nav]').contains("My Songs");
-    cy.get('[data-cy=song-book-nav]').contains("Song Book");
-    cy.get('[data-cy=search-form]').should('exist');
-    cy.get('[data-cy=song-card]').should("have.length", 2);
-    cy.get('[data-cy=song-card]').eq(0).contains("Radiohead");
-    cy.get('[data-cy=song-card]').eq(1).contains("Usher");
-    cy.get('[data-cy=song-card]').first().should("contain", "Jigsaw falling into place")
-    .and("contain", "Radiohead")
-    .and("contain","Electronica")
-    .and("contain","Rock")
-  cy.get('[data-cy=album-img]').first().should('exist');
-  cy.get('[data-cy=song-card-btn]').first().should('exist');
-  cy.get('[data-cy=microphone-icon]').should('exist');
+    //still need this functionality
+    it('should display a helpful message if search yields no results', () => {
+      expect(true).to.equal(false);
+    });
+
+    it('should allow a user to add a song to their library', () => {
+      cy.get('[data-cy=song-card-btn]').first().click();
+      cy.get('[data-cy=my-songs-nav]').click();
+      cy.get('[data-cy=song-card]').should('contain', 'Jigsaw falling into place')
+          .and('contain', 'Radiohead')
+          .and('contain','Electronica')
+          .and('contain','Rock');
+    });
+
+    it('should not allow user to add duplicate songs to user library', () => {
+      cy.get('[data-cy=song-card-btn]').first().click();
+      cy.get('[data-cy=song-card-btn]').first().should('be.disabled');
+      cy.get('[data-cy=heart-icon]').should('exist');
+    });
   });
 
-  it('should allow user to make a search', () => {
-    cy.get('[data-cy=search-bar]').type('usher');
-    cy.get('[data-cy=search-btn').click()
-    cy.get('[data-cy=song-card]').should("have.length", 1)
-      .and('contain', 'Usher');
-    cy.get('[data-cy=search-bar]').clear()
-    cy.get('[data-cy=search-bar]').type('body')
-    cy.get('[data-cy=search-btn').click()
-    cy.get('[data-cy=song-card]').should('have.length', 2);
-  });
+  describe('Lyrics card', () => {
+    beforeEach(() => {
+      cy.intercept( '/genres', {fixture:'genre_data.json'});
+      cy.intercept('/songs', {fixture:'song_data.json'});
+      cy.visit('http://localhost:3000');
+      cy.get('[data-cy=song-book-nav]').click();
+    });
 
-  //still need this functionality
-  it('should display a helpful message if search yields no results', () => {
-    expect(true).to.equal(false);
-  });
-
-  it('should allow a user to add a song to their library', () => {
+    it('should display a lyric card for all views', () => {
+    cy.get('[data-cy=microphone-icon]').first().click();
+    cy.get('[data-cy=lyric-content]').contains('Just as you take my hand');
+    cy.get('[data-cy=lyric-close-icon]').click();
     cy.get('[data-cy=song-card-btn]').first().click();
-    cy.get('[data-cy=my-songs-nav]').click()
-    cy.get('[data-cy=song-card]').should("contain", "Jigsaw falling into place")
-        .and("contain", "Radiohead")
-        .and("contain","Electronica")
-        .and("contain","Rock")
+    cy.get('[data-cy=my-songs-nav]').click();
+    cy.get('[data-cy=microphone-icon]').click();
+    cy.get('[data-cy=lyric-content]').contains('Just as you take my hand');
+
+    });
   });
-
-  it('should not allow user to add duplicate songs to user library', () => {
-    cy.get('[data-cy=song-card-btn]').first().click();
-    cy.get('[data-cy=song-card-btn]').first().should('be.disabled');
-    cy.get('[data-cy=heart-icon]').should('exist');
-  });
-});
-
-describe('Lyrics card', () => {
-  beforeEach(() => {
-    cy.intercept( '/genres', {fixture:"genre_data.json"})
-    cy.intercept('/songs', {fixture:"song_data.json"})
-    cy.visit('http://localhost:3000');
-    cy.get('[data-cy=song-book-nav]').click();
-   });
-
-   it('should display a lyric card for all views', () => {
-   cy.get('[data-cy=microphone-icon]').first().click();
-   cy.get('[data-cy=lyric-content]').contains('Just as you take my hand');
-   cy.get('[data-cy=lyric-close-icon]').click();
-   cy.get('[data-cy=song-card-btn]').first().click();
-   cy.get('[data-cy=my-songs-nav]').click();
-   cy.get('[data-cy=microphone-icon]').click();
-   cy.get('[data-cy=lyric-content]').contains('Just as you take my hand');
-
-   });
-
-});
-
 });
 
