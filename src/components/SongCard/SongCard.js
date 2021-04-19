8ff55862f3cecb8b453213b25a13b1b7170af699
Elizabeth Hahn <elizhahn87@gmail.com>
2021-04-19 14:32:18 -0600

    Fix formatting for SongCard return statement

--- src/components/SongCard/SongCard.js
+++ src/components/SongCard/SongCard.js
@@ -37,8 +37,12 @@ render() {
   });
 
   return (
-      <div className='song-card' data-cy="song-card">
-        <img src={ album_cover } data-cy="album-img" alt={ `${title} by ${artist} album cover art` }/>
+      <div className="song-card" data-cy="song-card">
+        <img 
+        src={ album_cover } 
+        data-cy="album-img" 
+        alt={ `${title} by ${artist} album cover art` }
+        />
         <article className="song-details">
           <h2>{ title }</h2>
           <p>Artist: { artist }</p>
@@ -46,10 +50,35 @@ render() {
             { listItems }
           </ul>
         </article>
-        <button className="handle-song-btn" aria-label="add to favorites button" disabled={ !isActive } id={ id } onClick={ () => handleSong(id) } data-cy="song-card-btn">{ isActive ? buttonIcon[0] : buttonIcon[1] }</button>
-        <button className="handle-song-btn lyrics-btn" onClick={() => this.getLyrics(artist, title)}>{ <GiMicrophone className="handle-song-icon" aria-label='see lyrics button' data-cy="microphone-icon"/> }</button>
-        { !!Object.keys(this.state.lyrics).length && <Lyric lyrics={ this.state.lyrics } closeLyrics={ this.closeLyrics } error={ this.state.error }/> }
-        { this.state.error && <Lyric lyrics={ this.state.lyrics } closeLyrics={ this.closeLyrics } error={ this.state.error }/> }
+        <button 
+        className="handle-song-btn" 
+        aria-label="add to favorites button" 
+        disabled={ !isActive } 
+        id={ id } 
+        onClick={ () => handleSong(id) } 
+        data-cy="song-card-btn">{ isActive ? buttonIcon[0] : buttonIcon[1] }
+        </button>
+        <button 
+        className="handle-song-btn lyrics-btn" 
+        onClick={() => this.getLyrics(artist, title)}>
+          { <GiMicrophone 
+          className="handle-song-icon" 
+          aria-label="see lyrics button" 
+          data-cy="microphone-icon"
+          /> }
+        </button>
+        { !!Object.keys(this.state.lyrics).length &&
+         <Lyric 
+         lyrics={ this.state.lyrics } 
+         closeLyrics={ this.closeLyrics } 
+         error={ this.state.error }
+         /> }
+        { this.state.error && 
+        <Lyric 
+        lyrics={ this.state.lyrics } 
+        closeLyrics={ this.closeLyrics } 
+        error={ this.state.error }
+        /> }
       </div>
 
   );
