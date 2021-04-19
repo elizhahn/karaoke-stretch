c6ee42c43b27f4d5c09aeaa822a0ca5f407f1e2f
Marika <marikashanahan@gmail.com>
2021-04-19 09:46:20 -0600

    Apply z-index and animation to marble class

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,14 +3,6 @@ import { NavLink } from 'react-router-dom';
 import './Navigation.scss'
 
 const Navigation = ({ dynamic }) => {
-  const determineDivider = () => {
-    if (dynamic !== 'home-nav') {
-      return ''
-    } else {
-      return ''
-    }
-  }
-
   const determineHomeLink = () => {
     if (dynamic !== 'home-nav') {
       return 'active-nav-bar'
@@ -19,17 +11,25 @@ const Navigation = ({ dynamic }) => {
     }
   }
 
+  const determineBackground = () => {
+    if (dynamic !== 'home-nav') {
+      return ''
+    } else {
+      return ''
+    }
+  }
+
   return (
     <nav className={ dynamic }>
       <div className="marbled"></div>
       <div className="home-btns home-my-songs">
-        <NavLink activeClassName="mysongs-active"  className={ determineHomeLink() } id="my-songs" to="/mysongs" data-cy="my-songs-nav">My Songs</NavLink>
+        <NavLink activeClassName="mysongs-active"  className={ determineHomeLink() } id="my-songs" to="/mysongs" data-cy="my-songs-nav">{`My\nSongs`}</NavLink>
       </div>
-      <div className={ determineDivider() }>
+      <div>
         {dynamic !== 'home-nav' && <NavLink to="/" className="home-link vinyl"/>}
       </div>
       <div className="home-btns home-song-book">
-        <NavLink activeClassName="songbook-active" className="home-link" id="song-book" to="/songbook" data-cy="song-book-nav">Song Book</NavLink>
+        <NavLink activeClassName="songbook-active" className="home-link" id="song-book" to="/songbook" data-cy="song-book-nav">Song{"\n"}Book</NavLink>
       </div>
     </nav>
   )
